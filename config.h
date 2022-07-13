87711c1f42d5d8c0f07ba5c6b69b7f4ed5f35429
bonet <bonet@debian-BULLSEYE-live-builder-AMD64>
2022-07-13 16:36:47 +0200

    add statuscmd patch

--- config.h
+++ config.h
@@ -30,6 +30,7 @@ static const unsigned int alphas[][3]      = {
 
 static const char *const autostart[] = {	
 	"picom", "-b",NULL,
+	"xrdb", "$HOME/.Xresources", NULL,
 	"feh","--bg-scale", "/home/bonet/Downloads/rain-drops.jpg",NULL,
 	"dwmblocks", NULL,
 	NULL /* terminate */
@@ -71,6 +72,8 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+#define STATUSBAR "dwmblocks"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -150,7 +153,9 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
